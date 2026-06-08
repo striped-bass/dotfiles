a2193b2168fe5a833671cc371d0ca854204cd0be
striped-bass <github.chaste299@passinbox.com>
2026-06-07 20:23:17 -0700

    Move fish config

--- components/fish/theme.fish
+++
@@ -1,9 +0,0 @@
-function theme --on-signal USR2
-    theme.sh < $COLORFILE
-end
-
-if status is-interactive
-	starship init fish | source &
-	theme
-    sttt scanline --scanline-reverse true -d 0.5
-end
