9ac85817576e087d9f4b88da6d756af6b4afbe58
striped-bass <github.chaste299@passinbox.com>
2026-07-05 13:16:37 -0700

    Remove hyprshade from screenshot program

--- .config/hypr/yorha_conf/programs.lua
+++ .config/hypr/yorha_conf/programs.lua
@@ -6,6 +6,4 @@ global terminalServer   = "foot -s"
 global unimatrixAngelic = terminal .. " -o  font=Angelic:size=12 unimatrix -c white -l a"
 global fileManager      = terminal .. " -e yazi"
 global lock             = "hyprlock"
-global screenshot       = 'hyprshade off && grim -g "$(agsv1 -b geom -c ~/.config/agsv1/windows/geom/geom.js)" - | wl-copy && notify-send "Screenshot copied to clipboard" && hyprshade on gridlines'
--- hyprshade off
--- hyprshade on gridlines
+global screenshot       = 'grim -g "$(agsv1 -b geom -c ~/.config/agsv1/windows/geom/geom.js)" - | wl-copy && notify-send "Screenshot copied to clipboard"'
