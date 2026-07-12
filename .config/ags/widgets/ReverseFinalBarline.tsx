5c5a9cba20f6549ed3040984019c639759cf7d11
striped-bass <github.chaste299@passinbox.com>
2026-07-11 21:01:22 -0700

    Style the reverse final barline via CSS

--- .config/ags/widgets/ReverseFinalBarline.tsx
+++ .config/ags/widgets/ReverseFinalBarline.tsx
@@ -4,16 +4,12 @@ export function ReverseFinalBarline() {
     return(  
         <box
             class="ReverseFinalBarline"
-            widthRequest={50}
             >
             <Gtk.Separator
                 class="ThickBarline"
-                widthRequest={12}
             />
             <Gtk.Separator
                 class="ThinBarline"
-                widthRequest={3}
-                marginStart={5}
             />
         </box>
     )
